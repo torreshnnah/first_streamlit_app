40dead4a7fafd424907ecd1e7285d5ce444f0c99
torreshnnah <150647712+torreshnnah@users.noreply.github.com>
2023-11-13 14:53:55 +0800

    Update streamlit_app.py
    fixed code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('Kale, Spinach & Rocket Smoothie')
-streamlit.write('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
